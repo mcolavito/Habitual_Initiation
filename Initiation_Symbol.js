2a2e78abc5671d205cda505d3ed71c25f3518ce8
Yue Du <duyuescience@gmail.com>
2020-12-15 18:56:07 -0500

    'map'

--- Initiation_Symbol.js
+++ Initiation_Symbol.js
@@ -3076,7 +3076,9 @@ function Init_StimRoutineBegin(trials) {
 
     symb_map_rnd = Math.floor(rng2 * num_symb) // random interger between 0 and num_symb - 1
     symb_map_ind = symb_perm[symb_map_rnd];
-    
+    console.log(rng2)
+    console.log(symb_map_ind)
+
     remap_pair_rnd = Math.floor(rng3 * remap_pairs.length)
     remap_pair_1 = remap_pairs[remap_pair_rnd];
     for (i = 0, _pj_a = 4; (i < _pj_a); i += 1) {
