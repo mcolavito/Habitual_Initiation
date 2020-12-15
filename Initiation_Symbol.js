207e7ccdbe3a6b34fcee6d6bfdd46600cc023cb7
Yue Du <duyuescience@gmail.com>
2020-12-15 16:10:49 -0500

    'myrng()'

--- Initiation_Symbol.js
+++ Initiation_Symbol.js
@@ -360,6 +360,7 @@ function experimentInit() {
   var seed2 = participant + 1; //determine symb-key map
   var myrng = new Math.seedrandom('hello.');   //use new here so it does not affect Math.random()
   console.log(myrng())
+  console.log(myrng.int32());
 
   //determine which group participants are in
   grp_list = permute([1, 2, 3, 4]);
