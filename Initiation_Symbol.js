fb230ab5d99d2a9ac4626cc59e123c187fbaa6d7
Yue Du <duyuescience@gmail.com>
2020-12-14 23:32:56 -0500

    'remove-console'

--- Initiation_Symbol.js
+++ Initiation_Symbol.js
@@ -3147,8 +3147,6 @@ function Init_StimRoutineBegin(trials) {
     remap_pair_1 = remap_pairs[(participant % n_map)];
     for (i = 0, _pj_a = 4; (i < _pj_a); i += 1) {
         if  (!(remap_pair_1.includes(i))) {
-            console.log(i)
-            console.log(i + 4)
             remap_pair_2.append((i + 4));
         }
     }
