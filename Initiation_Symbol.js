d8455f4d36b44f8c26194e51e0683bd34970478d
Yue Du <duyuescience@gmail.com>
2021-01-12 15:20:01 -0500

    'add-browser'

--- Initiation_Symbol.js
+++ Initiation_Symbol.js
@@ -126,36 +126,16 @@ psychoJS.start({
 
 // Array.prototype.append = [].push
 
-// permutation of arrays
-function permute(permutation) {
-  var length = permutation.length,
-      result = [permutation.slice()],
-      c = new Array(length).fill(0),
-      i = 1, k, p;
-
-  while (i < length) {
-    if (c[i] < i) {
-      k = i % 2 && c[i];
-      p = permutation[i];
-      permutation[i] = permutation[k];
-      permutation[k] = p;
-      ++c[i];
-      i = 1;
-      result.push(permutation.slice());
-    } else {
-      c[i] = 0;
-      ++i;
-    }
-  }
-  return result;
-}
-
 var frameDur;
 function updateInfo() {
+  const browser = detectBrowser(); // detect browser
+
   expInfo['date'] = util.MonotonicClock.getDateStr();  // add a simple timestamp
   expInfo['expName'] = expName;
   expInfo['psychopyVersion'] = '2020.1.3';
   expInfo['OS'] = window.navigator.platform;
+  expInfo['browser'] = browser;
+  expInfo['browserInfo'] = window.navigator.userAgent;
 
   // store frame rate of monitor if we can measure it successfully
   expInfo['frameRate'] = psychoJS.window.getActualFrameRate();
@@ -7391,60 +7371,68 @@ function quitPsychoJS(message, isCompleted) {
   if (psychoJS.experiment.isEntryEmpty()) {
     psychoJS.experiment.nextEntry();
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
   psychoJS.window.close();
   psychoJS.quit({message: message, isCompleted: isCompleted});
   
   document.body.style.cursor='auto';
 
   return Scheduler.Event.QUIT;
+}
+
+// detect what browser participants are using
+function detectBrowser() {    
+  const isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0; // Opera 8.0+
+  const isFirefox = typeof InstallTrigger !== 'undefined'; // Firefox 1.0+
+  const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification)); // Safari 3.0+ "[object HTMLElementConstructor]"
+  const isIE = /*@cc_on!@*/false || !!document.documentMode; // Internet Explorer 6-11
+  const isEdge = !isIE && !!window.StyleMedia; // Edge 20+
+  const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime); // Chrome 1 - 79
+  const isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1); // Edge (based on chromium) detection
+  const isBlink = (isChrome || isOpera) && !!window.CSS; // Blink engine detection
+
+  let browser;
+  if (isOpera)
+    browser = 'Opera';
+  else if (isFirefox)
+    browser = 'Firefox';
+  else if (isSafari)
+    browser = 'Safari';
+  else if (isIE)
+    browser = 'IE';
+  else if (isEdge)
+    browser = 'Edge';
+  else if (isChrome)
+    browser = 'Chrome';
+  else if (isEdgeChromium)
+    browser = 'Edge Chromium';
+  else if (isBlink)
+    browser = 'Blink';
+  else
+    browser = 'Could not identify browser';
+  return browser;
+}
+
+// permutation of arrays
+function permute(permutation) {
+  var length = permutation.length,
+      result = [permutation.slice()],
+      c = new Array(length).fill(0),
+      i = 1, k, p;
+
+  while (i < length) {
+    if (c[i] < i) {
+      k = i % 2 && c[i];
+      p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      result.push(permutation.slice());
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+  return result;
 }
