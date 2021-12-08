a6b8b326bdd6981cf0f2a142bc71226fa2e9cd6a
michael colavito <michaelcolavito@michaelvitosmbp.win.ad.jhu.edu>
2021-12-08 13:53:53 -0500

    testing

--- Initiation_Symbol.js
+++ Initiation_Symbol.js
@@ -206,8 +206,8 @@ var tr_block_new_stop = 0;
 
 var tr_hand_yes = 0;
 var rt_hand_yes = 0;
-var cr_old_yes = 1;
-var cr_new_yes = 1;
+var cr_old_yes = 0;
+var cr_new_yes = 0;
 var rt_yes = 1;
 var tr_old_pre_yes = 1;
 var tr_old_post_yes = 1;
