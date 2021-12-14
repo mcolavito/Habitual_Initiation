2a3ddfe6a986df13613f5c725f693a54195853b9
michael colavito <michaelcolavito@michaelvitosmbp.win.ad.jhu.edu>
2021-12-14 13:09:47 -0500

    remap on evens

--- Initiation_Symbol.js
+++ Initiation_Symbol.js
@@ -3716,11 +3716,6 @@ function Creat_StimSeqRoutineBegin(trials) {
             symb = symb_remap;
             symb_g = symb_g_remap;
             symb_r = symb_r_remap;
-        } else {
-            symb = symb_map;
-            symb_g = symb_g_map;
-            symb_r = symb_r_map;
-        }
     }
 
     // keep track of which components have finished
