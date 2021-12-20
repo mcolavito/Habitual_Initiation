9b303126b7785f22f25ac62546faaacc77a509be
michael colavito <michaelcolavito@michaelvitosmbp.win.ad.jhu.edu>
2021-12-20 15:20:58 -0500

    remap try again

--- Initiation_Symbol.js
+++ Initiation_Symbol.js
@@ -3722,11 +3722,11 @@ function Creat_StimSeqRoutineBegin(trials) {
             symb_g = symb_g_map;
             symb_r = symb_r_map;
         } else {
-            if (block_count % 2 === 0) {
+            //if (block_count % 2 === 0) {
                 symb = symb_remap;
                 symb_g = symb_g_remap;
                 symb_r = symb_r_remap;
-            }
+            //}
         }
 
     }
