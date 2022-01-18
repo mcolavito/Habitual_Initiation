719b452ecc89143b1faf3f1b14278756d0cbb034
michael colavito <michaelcolavito@Michael-Colavitos-MacBook-Pro.local>
2022-01-18 15:58:13 -0500

    parameters fix

--- Initiation_Symbol.js
+++ Initiation_Symbol.js
@@ -198,20 +198,20 @@ var tr_block_hand = 4;
 var num_trials_hand = 96;
 var num_trials_cr = 2000;
 var num_criterion = 5;
-var num_trials = 96;
+var num_trials = 10;
 var rt_block = 12;
 var tr_block_old = 2;
 var tr_block_new_swap = 0;
 var tr_block_new_stop = 0;
 
-var tr_hand_yes = 1;
-var rt_hand_yes = 1;
-var cr_old_yes = 1;
-var cr_new_yes = 1;
+var tr_hand_yes = 0;
+var rt_hand_yes = 0;
+var cr_old_yes = 0;
+var cr_new_yes = 0;
 var rt_yes = 1;
-var tr_old_pre_yes = 1;
-var tr_old_post_yes = 1;
-var tr_new_yes = 1;
+var tr_old_pre_yes = 0;
+var tr_old_post_yes = 0
+var tr_new_yes = 0;
 //////////////////////////////////////
 
 var pre = 0;
